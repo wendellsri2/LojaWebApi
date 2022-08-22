dd813f732cd6c9c47e63ad8146e6b25b6c0f3e90
Wendell <wendellsri2@hotmail.com>
2022-08-22 17:30:20 -0300

    Criação da classe Startup

---
+++ LojaWebApi/Startup.cs
@@ -0,0 +1,82 @@
+﻿using LojaWebApi.Data;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace LojaWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            _ = services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            _ = services.AddDbContext<LojaWebApiContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("LojaWebApiContext"),
+                new MySqlServerVersion(new Version())
+                ));
+
+            _ = services.AddMvc(opção => opção.EnableEndpointRouting = false);
+
+            _ = services.AddScoped<LojaWebApiSevice>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [Obsolete]
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, LojaWebApiSevice lojawebapiService)
+        {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                lojawebapiService.Seed();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            _ = app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            
+        }
+    }
+}
