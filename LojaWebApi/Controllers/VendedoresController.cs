8a65076292159d85e010e1f45710623fc0166fdd
Wendell <wendellsri2@hotmail.com>
2022-08-24 10:07:41 -0300

    criando controller vendedores

---
+++ LojaWebApi/Controllers/VendedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaWebApi.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- LojaWebApi/Startup.cs
+++ LojaWebApi/Startup.cs
@@ -45,12 +45,12 @@ namespace LojaWebApi
         [Obsolete]
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, LojaWebApiSevice lojawebapiService)
         {
-            var enUS = new CultureInfo("en-US");
+            var ptBR = new CultureInfo("pt-BR");
             var localizationOptions = new RequestLocalizationOptions
             {
-                DefaultRequestCulture = new RequestCulture(enUS),
-                SupportedCultures = new List<CultureInfo> { enUS },
-                SupportedUICultures = new List<CultureInfo> { enUS }
+                DefaultRequestCulture = new RequestCulture(ptBR),
+                SupportedCultures = new List<CultureInfo> { ptBR },
+                SupportedUICultures = new List<CultureInfo> { ptBR }
             };
 
             app.UseRequestLocalization(localizationOptions);
