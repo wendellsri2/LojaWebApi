ac543060de9e2ccc436901f8992eaf4765b77c2b
Wendell <wendellsri2@hotmail.com>
2022-08-24 11:09:35 -0300

    atualizando a controller vendedor

--- LojaWebApi/Controllers/VendedoresController.cs
+++ LojaWebApi/Controllers/VendedoresController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LojaWebApi.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LojaWebApi.Controllers
 {
     public class VendedoresController : Controller
     {
+        private readonly VendedorService _vendedorService;
+
+        public VendedoresController(VendedorService vendedorService)
+        {
+            _vendedorService = vendedorService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _vendedorService.Listar();
+            return View(list);
         }
     }
 }
