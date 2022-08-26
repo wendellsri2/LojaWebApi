7f3f95a45c1252df12dd3419a98ec065924755bb
Wendell <wendellsri2@hotmail.com>
2022-08-26 11:34:02 -0300

    adicionando metodos no serviço

--- LojaWebApi/Services/VendedorService.cs
+++ LojaWebApi/Services/VendedorService.cs
@@ -1,5 +1,6 @@
 ﻿using LojaWebApi.Data;
 using LojaWebApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LojaWebApi.Services
 {
@@ -15,6 +16,22 @@ namespace LojaWebApi.Services
         public List<Vendedor> Listar()
         {
             return _context.Vendedor.ToList();
+        }
+        public void Inserir(Vendedor obj)
+        {            
+            _context.Add(obj);
+            _ = _context.SaveChanges();
+        }
+        public Vendedor Procurar(int id)
+        {
+            return _context.Vendedor.Include(obj => obj.Departamento).FirstOrDefault(obj => obj.VendedorId == id);
+        }
+
+        public void Remover(int id)
+        {
+            var obj = _context.Vendedor.Find(id);
+            _ = _context.Vendedor.Remove(obj);
+            _ = _context.SaveChanges();
         }
     }
 }
