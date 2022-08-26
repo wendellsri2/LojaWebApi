747ed27be385bddf0616b0e54fb5461148511277
Wendell <wendellsri2@hotmail.com>
2022-08-26 11:33:27 -0300

    criando serviço departamento

---
+++ LojaWebApi/Services/DepartamentoService.cs
@@ -0,0 +1,20 @@
+﻿using LojaWebApi.Data;
+using LojaWebApi.Models;
+
+namespace LojaWebApi.Services
+{
+    public class DepartamentoService
+    {
+        private readonly LojaWebApiContext _context;
+
+        public DepartamentoService(LojaWebApiContext context)
+        {
+            _context = context;
+        }
+
+        public List<Departamento> ListarAll()
+        {
+            return _context.Departamento.OrderBy(x => x.Nome).ToList();
+        }
+    }
+}
