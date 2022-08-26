136ea778249bf9f73254f1c92a143857c053349c
Wendell <wendellsri2@hotmail.com>
2022-08-26 16:15:31 -0300

    formatando campos para mostrar na tela

--- LojaWebApi/Models/Vendedor.cs
+++ LojaWebApi/Models/Vendedor.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 namespace LojaWebApi.Models
@@ -8,8 +9,16 @@ namespace LojaWebApi.Models
     {
         public int VendedorId { get; set; }
         public string? Nome { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
+
+        [Display(Name = "Data Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime DtNascimento { get; set; }
+
+        [Display(Name = "Base Salarial")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalarial { get; set; }
         public Departamento? Departamento { get; set; }
         public int DepartamentoId { get; set; }
