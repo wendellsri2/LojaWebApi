cd9d66d4666a029f50c7fbc92239142c21921d1a
Wendell <wendellsri2@hotmail.com>
2022-08-26 11:29:05 -0300

    atualizando a controller departamento

--- LojaWebApi/Controllers/DepartamentoController.cs
+++ LojaWebApi/Controllers/DepartamentoController.cs
@@ -56,7 +56,7 @@ namespace LojaWebApi.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("DepartamentoId,Nome")] Departamento departamento)
+        public async Task<IActionResult> Create([Bind("Id,Nome")] Departamento departamento)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace LojaWebApi.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("DepartamentoId,Nome")] Departamento departamento)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome")] Departamento departamento)
         {
             if (id != departamento.DepartamentoId)
             {
