6bb0f166965160b60fa217fe93f21b4f85c8152d
Wendell <wendellsri2@hotmail.com>
2022-08-26 11:30:23 -0300

    Desenvolvendo campo create e delete

--- LojaWebApi/Controllers/VendedoresController.cs
+++ LojaWebApi/Controllers/VendedoresController.cs
@@ -1,4 +1,6 @@
-﻿using LojaWebApi.Services;
+﻿using LojaWebApi.Models;
+using LojaWebApi.Models.ViewModels;
+using LojaWebApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LojaWebApi.Controllers
@@ -6,16 +8,67 @@ namespace LojaWebApi.Controllers
     public class VendedoresController : Controller
     {
         private readonly VendedorService _vendedorService;
+        private readonly DepartamentoService _departamentoService;
 
-        public VendedoresController(VendedorService vendedorService)
+        public VendedoresController(VendedorService vendedorService, DepartamentoService departamentoService)
         {
             _vendedorService = vendedorService;
+            _departamentoService = departamentoService;
         }
 
         public IActionResult Index()
         {
             var list = _vendedorService.Listar();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            var departamentos = _departamentoService.ListarAll();
+            var viewModel = new VendedorFormViewModel { Departamentos = departamentos };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Vendedor vendedor)
+        {
+            _vendedorService.Inserir(vendedor);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _vendedorService.Procurar(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _vendedorService.Remover(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _vendedorService.Procurar(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
         }
     }
 }
