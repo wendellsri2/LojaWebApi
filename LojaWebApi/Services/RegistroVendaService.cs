36d18ca303473d87f750230d848955b1ce54e3c1
Wendell <wendellsri2@hotmail.com>
2022-08-30 17:52:52 -0300

    implementando o serviço de registro de vendas

---
+++ LojaWebApi/Services/RegistroVendaService.cs
@@ -0,0 +1,55 @@
+﻿using LojaWebApi.Data;
+using LojaWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LojaWebApi.Services
+{
+    public class RegistroVendaService
+    {
+        private readonly LojaWebApiContext _context;
+
+        public RegistroVendaService(LojaWebApiContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<RegistroVenda>> ProcurarDataAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.RegistroVenda select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.DataVenda >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.DataVenda <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Vendedor)
+                .Include(x => x.Vendedor.Departamento)
+                .OrderByDescending(x => x.DataVenda)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Departamento, RegistroVenda>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.RegistroVenda select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.DataVenda >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.DataVenda <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Vendedor)
+                .Include(x => x.Vendedor.Departamento)
+                .OrderByDescending(x => x.DataVenda)
+                .GroupBy(x => x.Vendedor.Departamento)
+                .ToListAsync();
+        }
+    }
+}
